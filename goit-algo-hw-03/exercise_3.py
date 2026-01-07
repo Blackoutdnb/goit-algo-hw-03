b60fe5e2b76d4ce6f18908272e528cf2ebf04329
Yevhen Moshnyk <blackoutdnb@gmail.com>
2026-01-07 12:42:45 +0100

    fix exercise 3

--- goit-algo-hw-03/exercise_3.py
+++ goit-algo-hw-03/exercise_3.py
@@ -1,8 +1,13 @@
-я попробую выслать тебе весь код целиком
-def read_text_file(path):
-try:
-withopen(path, encoding="utf-8") as f:
-path = f.read()
-except (UnicodeDecodeError, FileNotFoundError): 
-path = ""
-return path
+import re
+
+def normalize_phone(phone_number):
+    cleaned_number = re.sub("[^0-9+]", "", phone_number)
+
+    if cleaned_number.startswith("+"):
+        cleaned_number = cleaned_number
+    elif cleaned_number.startswith("380"):
+        cleaned_number = "+" + cleaned_number
+    else:
+        cleaned_number = "+38" + cleaned_number
+
+    return cleaned_number
